d4339177545bffc61780138040399da8d1a88762
JulietaMontenegro <101186383+JulietaMontenegro@users.noreply.github.com>
2023-05-09 15:58:35 -0300

    Update carrusel.js

--- carrusel.js
+++ carrusel.js
@@ -1,22 +1,50 @@
-
+var contador=0;
+var children;
 function activarMiCarrusel(carrusel, tiempo){
-    var children = document.getElementById(carrusel).getElementsByTagName('img');
+    children = document.getElementById(carrusel).getElementsByTagName('img');
     var long= children.length;
     for(var i=0; i<long; i++){
         children[i].style="display:none";
     }
-    for(var i=0; i< long; i++){
-        setInterval(()=>{
-        console.log(i);
-        ocultar(children, i);
-        children[i].style="display:block";
-    }, tiempo);
- }
+    
+        setInterval(mostrar, tiempo);
+   
  }
 
  function ocultar(children, i){
- children[i-1].style="display:none";
+ children[i].style="display:none";
  }
+ function mostrar(){
+        console.log(contador);
+        ocultar(children, contador);
+        children[contador].style="display:none";
+        contador=contador+1;
+        if(contador == children.length){
+            contador=0;
+        }
+        children[contador].style="display:block";
+    
+ }
+ function anterior(){
+    ocultar(children, contador);
+    children[contador].style="display:none";
+        contador=contador-1;
+        if(contador-1 < 0){
+            contador=children.length-1;
+        }
+        children[contador].style="display:block";
+ }
+
+ function siguiente(){
+    ocultar(children, contador);
+    children[contador].style="display:none";
+        contador=contador+1;
+        if(contador+1 > children.length){
+            contador=0;
+        }
+        children[contador].style="display:block";
+ }
+
 
 
 
